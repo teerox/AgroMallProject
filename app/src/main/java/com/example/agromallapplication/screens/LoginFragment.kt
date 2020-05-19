39409a115fd4f96f28388df5311061dfbcec070b
Odu Anthony <teerox44@gmail.com>
2020-05-19 12:23:36 +0100

    Implement Login page with functionalities

--- app/src/main/java/com/example/agromallapplication/screens/LoginFragment.kt
+++ app/src/main/java/com/example/agromallapplication/screens/LoginFragment.kt
@@ -2,11 +2,14 @@ package com.example.agromallapplication.screens
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.example.agromallapplication.R
 import com.example.agromallapplication.databinding.FragmentLoginBinding
 import kotlinx.android.synthetic.main.fragment_login.*
@@ -25,39 +28,42 @@ class LoginFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_login,container,false)
         hideView(binding.wrongEmail,binding.wrongPassword)
 
-        val userEmail = binding.email.text.toString()
-        val userPassword = binding.password.text.toString()
 
         //Login button
         binding.login.setOnClickListener {
-            val validate = login(userEmail,userPassword)
-            if (validate){
+            val userEmail = binding.email.text.toString()
+            val userPassword = binding.password.text.toString()
+            Log.e("Info",userEmail)
+            Log.e("Info",userPassword)
+
+            if (userEmail == "test@theagromall.com " && userPassword == "password"){
                 //move to the dashBoard
+                val navigate = LoginFragmentDirections.actionLoginFragmentToDashBoardFragment2()
+                findNavController().navigate(navigate)
                 hideView(binding.wrongEmail,binding.wrongPassword)
             }else{
                 displayView(binding.wrongEmail,binding.wrongPassword)
             }
         }
 
-
         return binding.root
     }
 
 
-    private fun login(email:String, password:String):Boolean{
-        return email == "test@theagromall.com" && password == "password"
-    }
-
 
     private fun displayView(emailView: View, passwordView: View){
+
         emailView.visibility = View.VISIBLE
         passwordView.visibility = View.VISIBLE
+
     }
 
 
     private fun hideView(emailView: View, passwordView: View){
+
         emailView.visibility = View.GONE
         passwordView.visibility = View.GONE
+
     }
 
 
